1446314697fe47e1c6899b414702c31ee0cc00ce
SThevenot <sktmh007@gmail.com>
2023-01-10 14:51:35 -0600

    added footer and nav bar components

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import HomePage from "./pages/HomePage";
 import Members from "./pages/Members";
 import UpcomingEvents from "./pages/UpcomingEvents";
 import ContactUs from "./pages/ContactUs";
+import NavTab from "./components/NavTab";
+import Footer from "./components/Footer";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <NavTab pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
       <Routes>
         <Route exact path="/" element={<HomePage />}></Route>
         <Route exact path="/members" element={<Members />}></Route>
@@ -19,6 +22,9 @@ function App() {
         ></Route>
         <Route exact path="/contact-us" element={<ContactUs />}></Route>
       </Routes>
+      <section id="footerSection">
+        <Footer />
+      </section>
     </Router>
   );
 }
