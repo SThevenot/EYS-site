ee69126669566de46079539054f96752a50b7a53
SThevenot <sktmh007@gmail.com>
2023-01-10 14:49:20 -0600

    set up app.js to render home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 /** @format */
 import HomePage from "./pages/HomePage";
+import Members from "./pages/Members";
+import UpcomingEvents from "./pages/UpcomingEvents";
+import ContactUs from "./pages/ContactUs";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div id="main-div"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />}></Route>
+        <Route exact path="/members" element={<Members />}></Route>
+        <Route
+          exact
+          path="/upcoming-events"
+          element={<UpcomingEvents />}
+        ></Route>
+        <Route exact path="/contact-us" element={<ContactUs />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
