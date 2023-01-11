46f5f8d4120e576f38302e0cfb07edbb298f6213
SThevenot <sktmh007@gmail.com>
2023-01-11 13:38:41 -0600

    added social links and icons

--- client/src/components/NavTab.js
+++ client/src/components/NavTab.js
@@ -2,6 +2,8 @@
 
 import React from "react";
 import Hamburger from "./Hamburger";
+import {BsFacebook, BsInstagram} from "react-icons/bs";
+
 
 function NavTab() {
   return (
@@ -9,8 +11,16 @@ function NavTab() {
       <div id="navHeaderDiv">
       <h2>EYS  |  MNY</h2>
       <ul id="navList">
-        <li id="navListItem">Facebook</li>
-        <li id="navListItem">Instagram</li>
+        <li id="navListItem"><button>
+          <a href="https://www.facebook.com/217kpop/" target="_blank" rel="noreferrer">
+            <BsFacebook />
+          </a>
+        </button></li>
+        <li id="navListItem"><button>
+          <a href="https://www.instagram.com/eysdance/?hl=en" target="_blank" rel="noreferrer">
+            <BsInstagram />
+          </a>
+        </button></li>
       </ul>
       </div>
       <Hamburger/>
