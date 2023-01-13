66f9529c9b0ad57e227c4d632dff1df6e656561b
SThevenot <sktmh007@gmail.com>
2023-01-13 10:14:28 -0600

    added better images and fixed size

--- client/src/components/NavTab.js
+++ client/src/components/NavTab.js
@@ -2,28 +2,44 @@
 
 import React from "react";
 import Hamburger from "./Hamburger";
-import {BsFacebook, BsInstagram} from "react-icons/bs";
-
+import { BsFacebook, BsInstagram } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
 
 function NavTab() {
   return (
     <div id="navTabSection">
       <div id="navHeaderDiv">
-      <h2>EYS  |  MNY</h2>
-      <ul id="navList">
-        <li id="navListItem"><button>
-          <a href="https://www.facebook.com/217kpop/" target="_blank" rel="noreferrer">
-            <BsFacebook />
-          </a>
-        </button></li>
-        <li id="navListItem"><button>
-          <a href="https://www.instagram.com/eysdance/?hl=en" target="_blank" rel="noreferrer">
-            <BsInstagram />
-          </a>
-        </button></li>
-      </ul>
+        <h2>
+          <NavLink to="/EYS-site" id="title">
+            EYS | MNY
+          </NavLink>
+        </h2>
+        <ul id="navList">
+          <li id="navListItem">
+            <button>
+              <a
+                href="https://www.facebook.com/217kpop/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BsFacebook />
+              </a>
+            </button>
+          </li>
+          <li id="navListItem">
+            <button>
+              <a
+                href="https://www.instagram.com/eysdance/?hl=en"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BsInstagram />
+              </a>
+            </button>
+          </li>
+        </ul>
       </div>
-      <Hamburger/>
+      <Hamburger />
     </div>
   );
 }
