f3ef3611585e43cf56910defadcfbaead8a762fd
SThevenot <sktmh007@gmail.com>
2023-01-13 13:06:42 -0600

    created upcoming events with styling

--- client/src/pages/UpcomingEvents.js
+++ client/src/pages/UpcomingEvents.js
@@ -2,11 +2,76 @@
 
 import React from "react";
 import "../styles/UpcomingEvents.css";
+import { BsCalendar4Event } from "react-icons/bs";
 
 function UpcomingEvents() {
   return (
     <div id="upcomingEventsSection">
-      <h2>Upcoming Events</h2>
+      <div id="listDiv">
+        <h3>Upcoming Events</h3>
+        <ul>
+          <li id="eventsListItem">
+            <div>
+              <button>
+                <a
+                  href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NG4ydm9oZ2RnMzlicHJrb3J0bDQ0aG41NWogc2t0bWgwMDdAbQ&tmsrc=sktmh007%40gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsCalendar4Event />
+                </a>
+              </button>
+              <h4>January 20th, 2023</h4>
+              <h6>MOONLIGHT SUNRISE Album Release</h6>
+            </div>
+          </li>
+          <li id="eventsListItem">
+            <div>
+              <button>
+                <a
+                  href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NG4ydm9oZ2RnMzlicHJrb3J0bDQ0aG41NWogc2t0bWgwMDdAbQ&tmsrc=sktmh007%40gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsCalendar4Event />
+                </a>
+              </button>
+              <h4>January 20th, 2023</h4>
+              <h6>MOONLIGHT SUNRISE Album Release</h6>
+            </div>
+          </li>
+          <li id="eventsListItem">
+            <div>
+              <button>
+                <a
+                  href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NG4ydm9oZ2RnMzlicHJrb3J0bDQ0aG41NWogc2t0bWgwMDdAbQ&tmsrc=sktmh007%40gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsCalendar4Event />
+                </a>
+              </button>
+              <h4>January 20th, 2023</h4>
+              <h6>MOONLIGHT SUNRISE Album Release</h6>
+            </div>
+          </li>
+          <li id="eventsListItem">
+            <div>
+              <button>
+                <a
+                  href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NG4ydm9oZ2RnMzlicHJrb3J0bDQ0aG41NWogc2t0bWgwMDdAbQ&tmsrc=sktmh007%40gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsCalendar4Event />
+                </a>
+              </button>
+              <h4>January 20th, 2023</h4>
+              <h6>MOONLIGHT SUNRISE Album Release</h6>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
